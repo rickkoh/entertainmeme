94b428336aa9c872d437ebf237e2272d5c1a0255
rickkoh <realrickkoh@gmail.com>
2019-07-02 23:22:20 +0800

    implemented basic API

--- app/src/main/java/com/example/entertainmeme/MainActivity.java
+++ app/src/main/java/com/example/entertainmeme/MainActivity.java
@@ -5,23 +5,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import java.net.URL;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titleTextView;
 
     Button previousBtn;
     Button skipBtn;
     Button likeBtn;
     Button inventoryBtn;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        titleTextView = (TextView)findViewById(R.id.titleTextView);
+
+        previousBtn = (Button)findViewById(R.id.previousBtn);
+        skipBtn = (Button)findViewById(R.id.skipBtn);
+        likeBtn = (Button)findViewById(R.id.likeBtn);
         inventoryBtn = (Button)findViewById(R.id.inventoryBtn);
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                API();
+            }
+        });
+
+        likeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         inventoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +66,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+    }
+
+    public void API() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://meme-api.herokuapp.com/gimme";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        titleTextView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                titleTextView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
+
     }
 }
