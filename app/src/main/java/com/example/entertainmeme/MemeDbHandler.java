9757acd980aacfb8a17445b0574669c5f9342719
rickkoh <realrickkoh@gmail.com>
2019-07-03 23:12:36 +0800

    implemented SQLite db.

--- app/src/main/java/com/example/entertainmeme/MemeDbHandler.java
+++ app/src/main/java/com/example/entertainmeme/MemeDbHandler.java
@@ -7,8 +7,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
