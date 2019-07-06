5dd88b89778a0237cad7f2b1fab1fd829cf563ab
Harith Mohamad Noor <s10185542@connect.np.edu.sg>
2019-07-06 19:11:59 +0800

    Implemented listview for top memes

--- app/src/main/java/com/example/entertainmeme/Top100.java
+++ app/src/main/java/com/example/entertainmeme/Top100.java
@@ -2,12 +2,15 @@ package com.example.entertainmeme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Top100 extends AppCompatActivity {
+    ListView topListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top100);
+        topListView=(ListView)findViewById(R.id.topListView);
     }
 }
