023f31abbab7ace8d80cc0bc62e9883c88939121
rickkoh <realrickkoh@gmail.com>
2019-07-10 17:29:35 +0800

    updated variables and functions names.

--- app/src/main/java/com/example/entertainmeme/helpers/MemeLoader.java
+++ app/src/main/java/com/example/entertainmeme/helpers/MemeLoader.java
@@ -1,6 +1,5 @@
 package com.example.entertainmeme.helpers;
 import android.content.Context;
-import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -10,18 +9,12 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.entertainmeme.models.Meme;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Observable;
-import java.util.Queue;
 import java.util.Stack;
-
-import static android.content.ContentValues.TAG;
 
 // This class is a singleton class
 // It is responsible for handling the loading of memes
@@ -32,13 +25,11 @@ public class MemeLoader extends Observable {
     // Can only create one instance of MemeLoader
     private static MemeLoader memeLoader = null;
 
-    public RequestQueue requestQueue;
+    private static RequestQueue requestQueue;
 
-    // Implements LIFO data structure
-    private static Stack<Meme> previousMemes = new Stack<Meme>();
-
+    // List of memes loaded from the API
     private static List<Meme> memes = new ArrayList<Meme>();
-    private static int noOfMemesInQueue = 0;
+    private static int noOfPreloadedMemes = 0;
 
     // Instantiate MemeLoader
     private MemeLoader(Context context) {
@@ -59,33 +50,42 @@ public class MemeLoader extends Observable {
         return memeLoader;
     }
 
-    public static void loadMore() {
-        noOfMemesInQueue-=1;
+    //
+    public static void offloadMeme() {
+        if (noOfPreloadedMemes > 0) noOfPreloadedMemes-=1;
         memeLoader.loadMemes();
     }
 
-    public static int getNoOfMemesInQueue() {
-        return noOfMemesInQueue;
+    // Return memes
+    public List<Meme> getMemes() {
+        return memes;
     }
 
+    public static int getNoOfPreloadedMemes() {
+        return noOfPreloadedMemes;
+    }
+
+    // Load memes from API
     private void loadMemes() {
         String url = "https://meme-api.herokuapp.com/gimme";
-        if (noOfMemesInQueue >= 10) return;
+        if (noOfPreloadedMemes >= 10) return;
         StringRequest stringRequest = new StringRequest(
                 Request.Method.GET,
                 url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        try {
-                            JSONObject j = new JSONObject(response);
-                            memes.add(new Meme(j.getString("postLink"), j.getString("subreddit"), j.getString("title"), j.getString("url")));
-                            setChanged();
-                            notifyObservers();
-                            noOfMemesInQueue+=1;
-                            if (noOfMemesInQueue < 10) loadMemes();
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        if (noOfPreloadedMemes <= 10) {
+                            try {
+                                JSONObject j = new JSONObject(response);
+                                memes.add(new Meme(j.getString("postLink"), j.getString("subreddit"), j.getString("title"), j.getString("url")));
+                                setChanged();
+                                notifyObservers();
+                                noOfPreloadedMemes +=1;
+                                if (noOfPreloadedMemes < 10) loadMemes();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 },
@@ -96,9 +96,6 @@ public class MemeLoader extends Observable {
                 });
         requestQueue.add(stringRequest);
     }
-    
-    public List<Meme> getMemes() {
-        return memes;
-    }
+
 
 }
