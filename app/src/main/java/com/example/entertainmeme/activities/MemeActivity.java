888d58c9823a2041a252f7c9d0705b266af038db
rickkoh <realrickkoh@gmail.com>
2019-07-12 19:08:14 +0800

    migrated project to AndroidX.

--- app/src/main/java/com/example/entertainmeme/activities/MemeActivity.java
+++ app/src/main/java/com/example/entertainmeme/activities/MemeActivity.java
@@ -1,7 +1,7 @@
 package com.example.entertainmeme.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.entertainmeme.R;
--- app/src/main/java/com/example/entertainmeme/activities/MemeCardActivity.java
+++ app/src/main/java/com/example/entertainmeme/activities/MemeCardActivity.java
@@ -2,11 +2,10 @@ package com.example.entertainmeme.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.example.entertainmeme.R;
 
 public class MemeCardActivity extends AppCompatActivity {
