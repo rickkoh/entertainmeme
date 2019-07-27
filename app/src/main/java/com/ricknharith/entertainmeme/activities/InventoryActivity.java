19a07c50870b3887fd9a098189d819915f00e83d
harith mdnoor <harithmdnoor@gmail.com>
2019-07-27 14:28:44 +0800

    Added save and share functions

--- app/src/main/java/com/ricknharith/entertainmeme/activities/InventoryActivity.java
+++ app/src/main/java/com/ricknharith/entertainmeme/activities/InventoryActivity.java
@@ -1,24 +1,39 @@
 package com.ricknharith.entertainmeme.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.ricknharith.entertainmeme.R;
 import com.ricknharith.entertainmeme.helpers.MemeDbHelper;
 import com.ricknharith.entertainmeme.helpers.MemeLayoutAdapter;
 import com.ricknharith.entertainmeme.models.Meme;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InventoryActivity extends AppCompatActivity implements MemeLayoutAdapter.OnClickListener {
@@ -36,8 +51,7 @@ public class InventoryActivity extends AppCompatActivity implements MemeLayoutAd
     URI uri;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
 
         memeDbHelper = new MemeDbHelper(this);
@@ -70,6 +84,40 @@ public class InventoryActivity extends AppCompatActivity implements MemeLayoutAd
         startActivity(i);
     }
 
+    private String saveImage(Bitmap image,int position) {
+        String savedImagePath = null;
+
+        String imageFileName = "JPEG_" + memes.get(position).getTitle() + ".jpg";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/MemeFolder");
+        boolean success = true;
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs();
+        }
+        if (success) {
+            File imageFile = new File(storageDir, imageFileName);
+            savedImagePath = imageFile.getAbsolutePath();
+            try {
+                OutputStream fOut = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                fOut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Add the image to the system gallery
+            galleryAddPic(savedImagePath);
+            Toast.makeText(this, "IMAGE SAVED", Toast.LENGTH_LONG).show();
+        }
+        return savedImagePath;
+    }
+
+    private void galleryAddPic(String imagePath) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(imagePath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        sendBroadcast(mediaScanIntent);
+    }
     @Override
     public void onElipsisButtonClickListener(final View view, final int position) {
         PopupMenu popup = new PopupMenu(this, view);
@@ -78,13 +126,16 @@ public class InventoryActivity extends AppCompatActivity implements MemeLayoutAd
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.save_meme:
-                        return true;
                     case R.id.delete_meme:
                         memes.remove(position);
                         memeLayoutAdapter.notifyDataSetChanged();
                         return true;
                     case R.id.share_meme:
+                        Intent shareIntent = new Intent();
+                        shareIntent.setAction(Intent.ACTION_SEND);
+                        shareIntent.putExtra(Intent.EXTRA_STREAM,Uri.parse(memes.get(position).getUrl()));
+                        shareIntent.setType("image/jpeg");
+                        startActivity(Intent.createChooser(shareIntent, "Share images to.."));
                         return true;
                     default:
                         return false;
--- app/src/main/java/com/ricknharith/entertainmeme/activities/MemeActivity.java
+++ app/src/main/java/com/ricknharith/entertainmeme/activities/MemeActivity.java
@@ -1,6 +1,11 @@
 package com.ricknharith.entertainmeme.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -10,6 +15,7 @@ import com.github.chrisbanes.photoview.PhotoView;
 public class MemeActivity extends AppCompatActivity {
 
     PhotoView memePhotoView;
+    ImageButton shareBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class MemeActivity extends AppCompatActivity {
         // Point of this activity is to display the meme in full size
 
         memePhotoView = (PhotoView)findViewById(R.id.memePhotoView);
+        shareBtn = (ImageButton)findViewById(R.id.shareBtn);
 
         String title = getIntent().getStringExtra("title");
         String url = getIntent().getStringExtra("url");
@@ -28,6 +35,15 @@ public class MemeActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(url)
                 .into(memePhotoView);
-
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(getIntent().getStringExtra("url")));
+                shareIntent.setType("image/jpeg");
+                startActivity(Intent.createChooser(shareIntent, "Share images to.."));
+            }
+        });
     }
 }
